06fcfbfd2a3e3fac245ed9cdf6520ff51b43b0b7
rokhimin wahid <rokhim.whd@gmail.com>
2019-07-31 17:51:21 +0700

    #3 change prefix to !

--- config/config.rb
+++ config/config.rb
@@ -1,7 +1,7 @@
 #twitter.com/rokhiminwahid
 #twitter.com/whdzera
 configatron.token = "NjA1MzU2MjA5ANDBLAALALALALALALALALALALMONTOONBERCANDA" # go to http://discordapp.com/developers/applications/ to get your bot token
-configatron.prefix = "i?"
+configatron.prefix = "!"
 configatron.game = "check command bot | #{configatron.prefix}help"
 configatron.userid = 605373838837649152 # go to ur server -> right-click in ur profile -> copy id
 configatron.botname = "Indodax"
