3261fc1d82d468faf7ee7c039b8331c1a1cee136
rokhimin wahid <rokhim.whd@gmail.com>
2019-08-23 03:10:23 +0000

    #5 change prefix

--- config/config.rb
+++ config/config.rb
@@ -1,7 +1,7 @@
 
 #config
 configatron.site = "twitter.com/rokhiminwahid"
-configatron.token = "NjAzNjU2OTQ0MzM0NjY3Nzc2.XV9OXw.ReJkvMI5dns7UGycaJlQaMOIuu4"
+configatron.token = "token"
 configatron.prefix = ">"
 configatron.game = "#{configatron.prefix}help for commands"
 configatron.userid = "666"
